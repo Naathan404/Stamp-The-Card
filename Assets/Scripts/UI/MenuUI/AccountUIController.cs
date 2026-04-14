fe19f52297f52426f4be4753f535950ecfe52faf
Nguyen Nguyen <nathannguyen6002@gmail.com>
2026-04-14 23:37:01 +0700

    feat: audio manager

--- Assets/Scripts/UI/MenuUI/AccountUIController.cs
+++ Assets/Scripts/UI/MenuUI/AccountUIController.cs
@@ -5,5 +5,6 @@ public class AccountUIController : MonoBehaviour
     public void BackToMenu()
     {
         SceneTransitionManager.Instance.LoadSceneAsync("Menu");
+        AudioManager.Instance.PlaySFX(AudioManager.Instance.ButtonClick, true);
     }
 }
--- Assets/Scripts/UI/MenuUI/SettingUIController.cs
+++ Assets/Scripts/UI/MenuUI/SettingUIController.cs
@@ -4,6 +4,7 @@ public class SettingUIController : MonoBehaviour
 {
     public void BackToMenu()
     {
+        AudioManager.Instance.PlaySFX(AudioManager.Instance.ButtonClick, true);
         SceneTransitionManager.Instance.LoadSceneAsync("Menu");
     }
 }
